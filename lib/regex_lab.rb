310ed48d92b7a8556a09c375d48cb359952d25da
Kat B <katreesebarnes@gmail.com>
2019-03-22 17:05:38 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,10 +15,11 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/^\[A-Z].+[:punct:]$/) ? true : false
+  text.match(/^[[:upper:]].+[[:punct:]]$/) ? true : false
 
 end
 
 def valid_phone_number?(phone)
+  return phone.scan(/\d{3}\D{0,2}\d{3}\D{0,2}\d{4}/).length > 0 
 
 end
