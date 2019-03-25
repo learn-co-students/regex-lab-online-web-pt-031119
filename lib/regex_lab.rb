e3a6ac821d11673cb39868bcc7384740e7928657
AJ Guiles <ashiguiles@gmail.com>
2019-03-25 03:53:22 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,14 @@
 def starts_with_a_vowel?(word)
-
+ word.match(\b/[aeiou]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+  text.scan(\b\w+un\S*w+ing\b)
 
 end
 
 def words_five_letters_long(text)
-
+text.grep(/^\w{5}\s/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
